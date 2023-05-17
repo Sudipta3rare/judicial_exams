7ff06e7ab08c1c9a09de265b46f6ebf04e841965
user <yash.gupta@3raredynamics.com>
2023-05-17 18:30:48 +0530

    MakePayment screen list view donw

---
+++ lib/controller/start_exam_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class StartExam extends GetxController{
+  static StartExam to = Get.find();
+
+
+
+}
---
+++ lib/views/start_exam_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:judicial_exams/utils/styles.dart';
+class StartExamPage extends StatelessWidget {
+  const StartExamPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyle().backgroundColor,
+    );
+  }
+}
