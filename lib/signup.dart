a5f5ad80610eaf57e8aebbcfb02ee5cbc2bfe800
InfoTaxConnection <ratul007>
2023-05-19 00:58:25 -0700

    5

--- lib/signup.dart
+++ lib/signup.dart
@@ -2,187 +2,250 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:judicial_exams/utils/styles.dart';
 import 'package:judicial_exams/views/components/custom_button.dart';
 import 'package:get/get.dart';
-import 'package:judicial_exams/views/slider_pages/Slider1.dart';
+
 
 class MyRegister extends StatelessWidget {
   const MyRegister({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String email = '', pass = '';
+    String email = '', pass = '',phone = '',confirmPassword ='',fullname = '';
     return Scaffold(
      // backgroundColor: AppStyle().backgroundColor,
-      body: Container(
-        height: double.infinity,
-        padding: EdgeInsets.fromLTRB(32,0,32,0),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/background.png"),
-              fit: BoxFit.cover,
-              opacity: 0.1
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          padding: EdgeInsets.fromLTRB(32,50,32,0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/background.png"),
+                fit: BoxFit.cover,
+                opacity: 0.1
+            ),
           ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
 
-              // Padding(
-              //   padding: const EdgeInsets.all(8.0),
-              //   child: SvgPicture.asset(
-              //
-              //       width: MediaQuery.of(context).size.width,
-              //       height:400,
-              //       "assets/signUp.svg"),
-              // ),
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: SvgPicture.asset(
+                //
+                //       width: MediaQuery.of(context).size.width,
+                //       height:400,
+                //       "assets/signUp.svg"),
+                // ),
 
-              SizedBox(height: 150),
-
-              TextField(
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Full Name',
-                  labelStyle: TextStyle(color: Color(0xff3e1558)),
-                  border: OutlineInputBorder(borderSide: BorderSide(color: Color(0xff3e1558)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
-                  ),
-                  fillColor: Colors.white,  // Set the background color to white
-                  filled: true,
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text('Registration',style: GoogleFonts.montserrat(
+                    color: AppStyle().button,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                  ),),
                 ),
-              ),
 
-              SizedBox(height: 20),
-
-              TextField(
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Phone',
-                  labelStyle: TextStyle(color: Color(0xff3e1558)),
-                  border: OutlineInputBorder(borderSide: BorderSide(color: Color(0xff3e1558)),
+                SizedBox(height: 20),
+                TextField(
+                  onChanged: (value) {
+                    fullname = value;
+                  },
+                  decoration:  InputDecoration(
+                    labelText: 'Full Name',
+                    labelStyle: TextStyle(color:AppStyle().button),
+                    border: OutlineInputBorder(borderSide: BorderSide(color: AppStyle().button),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: AppStyle().button), // Change focused border color to red
+                    ),
+                    fillColor: Colors.white,  // Set the background color to white
+                    filled: true,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
-                  ),
-                  fillColor: Colors.white,  // Set the background color to white
-                  filled: true,
                 ),
-              ),
 
-              SizedBox(height: 20),
+                SizedBox(height: 20),
 
-              TextField(
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Color(0xff3e1558)),
-                  border: OutlineInputBorder(borderSide: BorderSide(color: Color(0xff3e1558)),
+                TextField(
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Color(0xff3e1558)),
+                    border: OutlineInputBorder(borderSide: BorderSide(color: Color(0xff3e1558)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
+                    ),
+                    fillColor: Colors.white,  // Set the background color to white
+                    filled: true,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
-                  ),
-                  fillColor: Colors.white,  // Set the background color to white
-                  filled: true,
                 ),
-              ),
 
-              SizedBox(height: 20),
+                SizedBox(height: 20),
 
-              TextField(
-                onChanged: (value) {
-                  pass = value;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Color(0xff3e1558)), // Change label text color to red
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff3e1558)), // Change border color to red
+                TextField(
+                  onChanged: (value) {
+                    phone = value;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Phone',
+                    labelStyle: TextStyle(color: Color(0xff3e1558)),
+                    border: OutlineInputBorder(borderSide: BorderSide(color: Color(0xff3e1558)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
+                    ),
+                    fillColor: Colors.white,  // Set the background color to white
+                    filled: true,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
-                  ),
-                  fillColor: Colors.white,  // Set the background color to white
-                  filled: true,
                 ),
-              ),
 
+                SizedBox(height: 20),
 
-              SizedBox(height: 20),
-
-
-              TextField(
-                onChanged: (value) {
-                  pass = value;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Confirm Password',
-                  labelStyle: TextStyle(color: Color(0xff3e1558)), // Change label text color to red
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff3e1558)), // Change border color to red
+                TextField(
+                  onChanged: (value) {
+                    pass = value;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Color(0xff3e1558)), // Change label text color to red
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff3e1558)), // Change border color to red
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
+                    ),
+                    fillColor: Colors.white,  // Set the background color to white
+                    filled: true,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
-                  ),
-                  fillColor: Colors.white,  // Set the background color to white
-                  filled: true,
                 ),
-              ),
 
 
-              SizedBox(height: 40),
+                SizedBox(height: 20),
 
-              GestureDetector(
-                onTap: () async {
-                  try {
-                    await Firebase.initializeApp(); // Ensure Firebase is initialized
-                    UserCredential userCredential =
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                      email: email,
-                      password: pass,
-                    );
-                    Navigator.pushNamed(context, 'home');
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'weak-password') {
-                      print('The password provided is too weak.');
-                    } else if (e.code == 'email-already-in-use') {
-                      print('The account already exists for that email.');
+
+                TextField(
+                  onChanged: (value) {
+                    confirmPassword = value;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Confirm Password',
+                    labelStyle: TextStyle(color: Color(0xff3e1558)), // Change label text color to red
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff3e1558)), // Change border color to red
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff3e1558)), // Change focused border color to red
+                    ),
+                    fillColor: Colors.white,  // Set the background color to white
+                    filled: true,
+                  ),
+                ),
+
+
+                SizedBox(height: 40),
+
+                GestureDetector(
+                  onTap: () async {
+
+                    if (fullname.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Please enter a valid full name.'),
+                          duration: const Duration(seconds: 2),
+                        ),
+                      );
+                      return;
                     }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-                child: CustomButton().customButton200(context, 'Sign Up'),
-              ),
-            SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Already have an account?"),
-                  SizedBox(width: 5),
-                  GestureDetector(
-                    onTap: () => Get.offAndToNamed('login'),
-                    child: const Text(
-                      "Sign In",
-                      style: TextStyle(
-                        color: Color(0xff3e1558),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+
+                    if (!RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$').hasMatch(email)) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Please enter a valid email.'),
+                          duration: const Duration(seconds: 2),
+                        ),
+                      );
+                    }
+
+                    if (phone.length != 10) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Please enter a valid phone number.'),
+                          duration: const Duration(seconds: 2),
+                        ),
+                      );
+                      return;
+                    }
+
+                    if (pass.length < 6) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Password should be at least 6 characters long.'),
+                          duration: const Duration(seconds: 2),
+                        ),
+                      );
+                      return;
+                    }
+
+                    if (pass != confirmPassword) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Passwords do not match.'),
+                          duration: const Duration(seconds: 2),
+                        ),
+                      );
+                      return;
+                    }
+
+                    try {
+                      await Firebase.initializeApp(); // Ensure Firebase is initialized
+                      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                        email: email,
+                        password: pass,
+                      );
+                      Navigator.pushNamed(context, 'home');
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'weak-password') {
+                        print('The password provided is too weak.');
+                      } else if (e.code == 'email-already-in-use') {
+                        print('The account already exists for that email.');
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                  child: CustomButton().customButton200(context, 'Sign Up'),
+                ),
+
+
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Already have an account?"),
+                    SizedBox(width: 5),
+                    GestureDetector(
+                      onTap: () => Get.offAndToNamed('login'),
+                      child: const Text(
+                        "Sign In",
+                        style: TextStyle(
+                          color: Color(0xff3e1558),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
