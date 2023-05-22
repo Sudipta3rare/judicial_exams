dd0fd9eb8a0bbc35ed82bcaa185e43c8cfadb407
user <yash.gupta@3raredynamics.com>
2023-05-22 18:17:26 +0530

    exam timer and submit timer activated

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,14 @@ import 'package:judicial_exams/controller/start_exam_controller.dart';
 import 'package:judicial_exams/controller/submit_exam_controller.dart';
 import 'package:judicial_exams/controller/timer_controller.dart';
 import 'package:get/get.dart';
+import 'package:judicial_exams/models/purchasedList_model.dart';
 import 'package:judicial_exams/signup.dart';
 import 'package:judicial_exams/views/dashboard_page/dashboard.dart';
+import 'package:judicial_exams/views/dashboard_page/pages/examlist.dart';
+import 'package:judicial_exams/views/dashboard_page/pages/ongoing_exams.dart';
+import 'package:judicial_exams/views/payment/payment_page.dart';
 import 'package:judicial_exams/views/slider_pages/Slider1.dart';
+import 'package:judicial_exams/views/start_exam_pages/submit_exam_page.dart';
 import 'login.dart';
 
 void main() async {
@@ -24,11 +29,11 @@ void main() async {
   // Get.lazyPut<TimerController>(() => TimerController(), fenix: true);
   Get.put(TimerController());
   Get.put(SubmitExamController());
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +45,11 @@ class MyApp extends StatelessWidget {
         'login': (context) => MyLogin(),
         'register': (context) => MyRegister(),
         'home': (context) => DashboardPage(),
+        'examList' : (context) => ExamList(),
+        'ongoingExams' : (context) => OngoingExams(),
+        'paymentPage' : (context) => PaymentPage(),
+        'submitExamPage' : (context) =>SubmitExamPage(),
+
       },
     ));
   }
