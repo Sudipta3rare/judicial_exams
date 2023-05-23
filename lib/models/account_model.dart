d650279a08ac9b4a4e982b6be2e1b417cb03e3a9
user <yash.gupta@3raredynamics.com>
2023-05-23 14:56:56 +0530

    account controller and model set

---
+++ lib/models/account_model.dart
@@ -0,0 +1,4 @@
+class AccountDetails {
+  AccountDetails({required this.name,required this.email,required this.phone});
+  String name,phone,email;
+}
--- lib/views/dashboard_page/pages/account.dart
+++ lib/views/dashboard_page/pages/account.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:judicial_exams/controller/account_controller.dart';
 import 'package:judicial_exams/controller/dashboard_controller.dart';
 import 'package:judicial_exams/login.dart';
 import 'package:judicial_exams/views/components/custom_button.dart';
@@ -10,27 +12,42 @@ import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import '../../../utils/styles.dart';
 class Account extends StatelessWidget {
   Account({Key? key}) : super(key: key);
-DashboardController dsCtrl = DashboardController.to;
-  final FirebaseAuth auth = FirebaseAuth.instance;
+
+
+
+  AccountPageController acCtrl = Get.put(AccountPageController());
 
   @override
   Widget build(BuildContext context) {
-
-    //signout function
-    signOut() async {
-     // dsCtrl.clickIndex.value =0;
-     dsCtrl.pTabController.index = 0;
-      await auth.signOut().then((value) => Get.offAndToNamed('login'));
-    }
-
       return Scaffold(
       backgroundColor:AppStyle().backgroundColor,
+
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (){
+            acCtrl.signOut();
+          }, icon: Icon(Icons.logout_outlined,
+          color: AppStyle().textHeading,))
+        ],
+        backgroundColor: AppStyle().secondaryColor,
+        title: Text('Account', style: GoogleFonts.montserrat(
+          color: AppStyle().textHeading,
+        ),),
+      ),
       body: SafeArea(
-        child: GestureDetector(
-            onTap: ()  {
-              signOut();
-            },
-            child: CustomButton().customButtonSmall(context, 'Sign Out')),
+       child: ListView(
+         children: [
+           Card(
+             child: Column(
+              children: [
+                Text('${acCtrl.accountDetail.name}'),
+                Text("Phone Number:")
+              ],
+
+             ),
+           )
+         ],
+       ),
       ),
     );
   }
