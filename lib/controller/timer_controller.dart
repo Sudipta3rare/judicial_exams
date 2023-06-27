2572abdc38166524bb1ce6d614516f9d1bf3e6f4
user <yash.gupta@3raredynamics.com>
2023-06-27 11:46:39 +0530

    account screen updated

--- lib/controller/timer_controller.dart
+++ lib/controller/timer_controller.dart
@@ -1,40 +1,35 @@
-
 import 'dart:async';
 
 import 'package:get/get.dart';
-import 'package:judicial_exams/views/dashboard_page/dashboard.dart';
-
 
 import '../views/start_exam_pages/pdf_view.dart';
 import '../views/start_exam_pages/submit_exam_page.dart';
 
-class TimerController extends GetxController{
-    static TimerController to = Get.find();
-
+class TimerController extends GetxController {
+  static TimerController to = Get.find();
 
   Timer? _timer;
   int remainSeconds = 1;
-  final RxString  time = '00.00'.obs;
+  final RxString time = '00.00'.obs;
   late double exTtime;
-    static const duration = Duration(seconds: 1);
+  static const duration = Duration(seconds: 1);
 
-   // Function()? onFinish;
+  // Function()? onFinish;
 
-  void startTimer(int seconds, Function onFinish){
-
+  void startTimer(int seconds, Function onFinish) {
     remainSeconds = seconds;
     _timer = Timer.periodic(duration, (Timer timer) {
-      if(remainSeconds<=0){
+      if (remainSeconds <= 0) {
         timer.cancel();
-          time.value = '00.00';
-          remainSeconds =1;
-          onFinish.call();
-
-      }else{
+        time.value = '00.00';
+        remainSeconds = 1;
+        onFinish.call();
+      } else {
         int hours = (remainSeconds ~/ 3600) % 60;
         int minutes = (remainSeconds ~/ 60) % 60;
-        int seconds = remainSeconds%60;
-        time.value = "${hours.toString().padLeft(2,"0")}:${minutes.toString().padLeft(2, "0")}:${seconds.toString().padLeft(2, "0")}";
+        int seconds = remainSeconds % 60;
+        time.value =
+            "${hours.toString().padLeft(2, "0")}:${minutes.toString().padLeft(2, "0")}:${seconds.toString().padLeft(2, "0")}";
         print(time.value);
         remainSeconds--;
       }
@@ -42,28 +37,28 @@ class TimerController extends GetxController{
   }
 
   @override
-  void onReady(){
+  void onReady() {
     // startTimer(exTtime);
     super.onReady();
   }
 
-  void onClose(){
-    if(_timer!=null){
+  void onClose() {
+    if (_timer != null) {
       _timer!.cancel();
-      remainSeconds =1;
-
+      remainSeconds = 1;
     }
     super.onClose();
   }
 
-  void goPdfView(double examTime, String path, String examName){
+  void goPdfView(double examTime, String path, String examName) {
     exTtime = examTime;
-    Get.offAll(()=>PdfViewPage(path: path, examName:   examName, examTime : examTime.toInt()));
+    Get.offAll(() => PdfViewPage(
+        path: path, examName: examName, examTime: examTime.toInt()));
 
-    remainSeconds=1;
-    time.value= '00:00';
-    startTimer(exTtime.toInt(), (){Get.offAll(SubmitExamPage());});
+    remainSeconds = 1;
+    time.value = '00:00';
+    startTimer(exTtime.toInt(), () {
+      Get.offAll(SubmitExamPage());
+    });
   }
-
-
 }
--- lib/views/exam_log_account/exam_log_page.dart
+++ lib/views/exam_log_account/exam_log_page.dart
@@ -5,7 +5,7 @@ import 'package:judicial_exams/controller/exam_log_controller.dart';
 import 'package:judicial_exams/utils/styles.dart';
 
 class ExamLogPage extends StatelessWidget {
-   ExamLogPage({Key? key}) : super(key: key);
+  ExamLogPage({Key? key}) : super(key: key);
 
   ExamLogController exCtrl = Get.put(ExamLogController());
 
@@ -15,35 +15,43 @@ class ExamLogPage extends StatelessWidget {
       backgroundColor: AppStyle().backgroundColor,
       appBar: AppBar(
         backgroundColor: AppStyle().secondaryColor,
-        title: Text("Exam Log", style:  GoogleFonts.montserrat(
-          color: AppStyle().textHeading,
-        )),
+        title: Text("Exam Log",
+            style: GoogleFonts.montserrat(
+              color: AppStyle().textHeading,
+            )),
       ),
       body: _showBody(context),
     );
   }
 
-  Widget _showBody(BuildContext context){
+  Widget _showBody(BuildContext context) {
     return ListView.separated(
       padding: EdgeInsets.all(10),
-        cacheExtent: 0,
-        itemCount: exCtrl.examLogList.length,
-        itemBuilder: (context, int index) => ListTile(
-          leading: CircleAvatar(
-            backgroundColor: AppStyle().secondaryColor,
-            child: Text(
-              '${index+1}',
-              style:GoogleFonts.montserrat(
-                color: AppStyle().textHeading
-              ),
-            ),
+      cacheExtent: 0,
+      itemCount: exCtrl.examLogList.length,
+      itemBuilder: (context, int index) => ListTile(
+        leading: CircleAvatar(
+          backgroundColor: AppStyle().secondaryColor,
+          child: Text(
+            '${index + 1}',
+            style: GoogleFonts.montserrat(color: AppStyle().textHeading),
           ),
-          // tileColor: AppStyle().textHeading,
-          contentPadding: EdgeInsets.all(5),
-      title: Text("Exam Name : ${exCtrl.examLogList[index].examName}", style: AppStyle().defaultText,),
-      trailing:  Text(exCtrl.examLogList[index].examMarks, style: AppStyle().subheadingBlack),
-      subtitle: Text("Exam Attended On : ${exCtrl.examLogList[index].examAttendedDate}" ,style: AppStyle().defaultText),
-    ), separatorBuilder: (BuildContext context, int index) {   return Divider(
-        thickness: 2); },);
+        ),
+        // tileColor: AppStyle().textHeading,
+        contentPadding: EdgeInsets.all(5),
+        title: Text(
+          "Exam Name : ${exCtrl.examLogList[index].examName}",
+          style: AppStyle().defaultText,
+        ),
+        trailing: Text(exCtrl.examLogList[index].examMarks,
+            style: AppStyle().subheadingBlack),
+        subtitle: Text(
+            "Exam Attended On : ${exCtrl.examLogList[index].examAttendedDate}",
+            style: AppStyle().defaultText),
+      ),
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider(thickness: 2);
+      },
+    );
   }
 }
